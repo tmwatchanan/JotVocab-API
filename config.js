f6e2478ca60f47c4730ffef130c5f8329ddb6259
tmwatchanan <tmwatchanan@gmail.com>
2017-11-27 17:05:29 +0700

    Make api respond to default endpoint /

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'port': 8080,
+    'secret': 'jotvocabbackendapiforcpe405finalproject',
+    'database': 'mongodb://watchanan:watchananchantapakul@ds011409.mlab.com:11409/jot-vocab'
+}
---
+++ index.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var app = express();
+
+var bodyParser = require('body-parser'); // handling HTML body
+var morgan = require('morgan'); // logging
+var mongoose = require('mongoose'); // Mongodb library
+var jwt = require('jsonwebtoken'); // token authentication
+
+var config = require('./config'); // global config
+
+var port = process.env.PORT || config.port // load port config
+
+//Set up default mongoose connection
+var mongoDB = config.database;
+mongoose.connect(mongoDB, {
+    useMongoClient: true
+});
+//Get the default connection
+var db = mongoose.connection;
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// use body parser so we can get info from POST and/or URL params
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+
+// use morgan to log requests to the console
+app.use(morgan('dev'));
+
+app.listen(port, () => {
+    console.log('Simple API started at http://localhost:' + port);
+});
+
+app.get('/', function (req, res) {
+    res.send('Hello! The API is at http://localhost:' + port + '/api');
+});
+
