b0151536150c2fe16333d9697585fdeccb21d48e
tmwatchanan <tmwatchanan@gmail.com>
2017-12-04 22:15:50 +0700

    Add headers for CORS

--- index.js
+++ index.js
@@ -17,12 +17,13 @@ app.set('port', port);
 var cors = require('cors');
 // Allow CORS
 app.use(cors());
-// app.use(function (req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// });
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 
 // app.set('superSecret', config.secret); // secret variable
 
